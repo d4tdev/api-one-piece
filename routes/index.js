64b10277ad3d7c2929e6e9aca4907ca69e6b7c57
d4rtj <jsmsj04@gmail.com>
2022-10-08 17:02:20 +0700

    fix router

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const mainController = require('../controllers/mainControllerV1.3');;
 
 router.get('/api/v1', mainController.getAllCharacters);
-router.get('api/v1/:name', mainController.getOneCharacter);
+router.get('/api/v1/:name', mainController.getOneCharacter);
 // router.get('/v1/:id/:name', mainController.getOneCharacter);
-router.get('api/v1/:name/Gallery', mainController.getGallery);
+router.get('/api/v1/:name/Gallery', mainController.getGallery);
 router.get('/', mainController.getHomePage)
 
 module.exports = router;
